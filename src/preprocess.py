ca8aeb83c67687de3a97d033545a3c8ca3165060
Jim Tang <jim.jim.tang@hotmail.com>
2023-06-30 23:15:59 +1000

    Add basic preprocessor and tokenizer

---
+++ src/preprocess.py
@@ -0,0 +1,97 @@
+import re
+import string
+from typing import List
+
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+
+# Can only be called once to make it thread-safe
+nltk.download("stopwords", quiet=True)
+nltk.download("wordnet", quiet=True)
+
+
+class Preprocessor:
+    def __init__(
+        self,
+        lowercase=True,
+        remove_non_ascii=True,
+        remove_punctuation=False,
+        lemmatization=True,
+        remove_stopwords=True,
+    ):
+        """Performs preprocessing and tokenizes the input
+
+        Args:
+            lowercase (bool, optional): Convert all ascii characters to lowercase. Defaults to True.
+            remove_non_ascii (bool, optional): Removes all non ascii characters from the input. Defaults to True.
+            remove_punctuation (bool, optional): Removes all punctuation. Defaults to False.
+            lemmatization (bool, optional): Use lemmatization, converting all words to their base form. Defaults to True.
+            remove_stopwords (bool, optional): Removes stopwords. Defaults to True.
+        """
+        self._lowercase = lowercase
+        self._remove_non_ascii = remove_non_ascii
+        self._remove_punctuation = remove_punctuation
+        if lemmatization:
+            self._lemmatizer = WordNetLemmatizer()
+        else:
+            self.lemmatizer = None
+        self._remove_stopwords = remove_stopwords
+    
+    def preprocess(self, input: str) -> str:
+        """Preprocess a string
+
+        Args:
+            input (str): The string to preprocess
+
+        Returns:
+            str: Preprocessed string
+        """
+        # Remove newlines
+        input.replace("\n", " ")
+
+        if self._remove_non_ascii or self._remove_punctuation:
+            def include_char(c):
+                if self._remove_non_ascii and c not in string.printable:
+                    return False
+                if self._remove_punctuation and c in string.punctuation:
+                    return False
+                return True
+            input = "".join(ch for ch in input if include_char(ch))
+        
+        if self._lemmatizer:
+            input = " ".join(map(self._lemmatizer.lemmatize, input.split()))
+
+        if self._lowercase:
+            input = input.lower()
+
+        if self._remove_stopwords:
+            words = stopwords.words("english")
+            input = " ".join(w for w in input.split() if w not in words)
+        
+        return input
+    
+    def tokenize(self, input: str) -> List[str]:
+        """Tokenize the input by splitting on punctuation. Punctuation will be
+        considered a token by themselves.
+
+        Args:
+            input (str): The string to tokenize
+
+        Returns:
+            List[str]: A list of tokens
+        """
+        return re.findall(r"\w+|[^\s\w]+", input)
+    
+    def preprocess_and_tokenize(self, input: str) -> List[str]:
+        return self.tokenize(self.preprocess(input))
+
+
+if __name__ == "__main__":
+    import sys
+    print("Text to extract preprocess: ", end="")
+    sys.stdout.flush()
+    input = sys.stdin.read()
+    processor = Preprocessor()
+    tokens = processor.preprocess_and_tokenize(input)
+    print(tokens)
