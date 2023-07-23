af71576597ebd05e66fd60bbcbe1c2c1866a71b5
Jim Tang <jim.jim.tang@hotmail.com>
2023-07-23 17:10:10 +1000

    classification: Support non-latent features

--- src/classification.py
+++ src/classification.py
@@ -1,4 +1,5 @@
 import logging
+from dataclasses import dataclass
 from pathlib import Path
 from typing import Any, Dict, Optional
 
@@ -9,7 +10,7 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import GridSearchCV, train_test_split
 from sklearn.naive_bayes import GaussianNB
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
 from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
 from tqdm import tqdm
@@ -17,15 +18,71 @@ from transformers import BertTokenizer, TFBertModel
 from xgboost import XGBClassifier
 
 from dataset import DatasetLoader
+from non_latent_features import NonLatentFeatures
 from preprocess import Preprocessor
-from sentiment import TextBlobSentimentExtractor
+# Sentiment comes from NonLatentFeatures now
+# from sentiment import TextBlobSentimentExtractor
 from textual_relevance import TextualRelevance
 
 
 class Pipeline:
-    def __init__(self, similarity=True, sentiment=False):
+    _DIVERSITY_KEYS = ["div_NOUN_sum", "div_NOUN_percent", "div_VERB_sum", "div_VERB_percent", "div_ADJ_sum", "div_ADJ_percent", "div_ADV_sum", "div_ADV_percent", "div_LEX_sum", "div_LEX_percent", "div_CONT_sum", "div_CONT_percent", "div_FUNC_sum", "div_FUNC_percent"]
+    _PRONOUN_KEYS = ["pron_FPS_sum", "pron_FPS_percent", "pron_FPP_sum", "pron_FPP_percent", "pron_STP_sum", "pron_STP_percent"]
+    _QUANTITY_KEYS = ["quant_NOUN_sum", "quant_NOUN_percent", "quant_VERB_sum", "quant_VERB_percent", "quant_ADJ_sum", "quant_ADJ_percent", "quant_ADV_sum", "quant_ADV_percent", "quant_PRON_sum", "quant_PRON_percent", "quant_DET_sum", "quant_DET_percent", "quant_NUM_sum", "quant_NUM_percent", "quant_PUNCT_sum", "quant_PUNCT_percent", "quant_SYM_sum", "quant_SYM_percent", "quant_PRP_sum", "quant_PRP_percent", "quant_PRP$_sum", "quant_PRP$_percent", "quant_WDT_sum", "quant_WDT_percent", "quant_CD_sum", "quant_CD_percent", "quant_VBD_sum", "quant_VBD_percent", "quant_STOP_sum", "quant_STOP_percent", "quant_LOW_sum", "quant_LOW_percent", "quant_UP_sum", "quant_UP_percent", "quant_NEG_sum", "quant_NEG_percent", "quant_QUOTE_sum", "quant_NP_sum", "quant_CHAR_sum", "quant_WORD_sum", "quant_SENT_sum", "quant_SYLL_sum"]
+    _SENTIMENT_KEYS = ["senti_!_sum", "senti_!_percent", "senti_?_sum", "senti_?_percent", "senti_CAPS_sum", "senti_CAPS_percent", "senti_POL_sum", "senti_SUBJ_sum"]
+    _AVERAGE_KEYS = ["avg_chars_per_word_sum", "avg_words_per_sent_sum", "avg_claus_per_sent_sum", "avg_puncts_per_sent_sum"]
+    _MEDIAN_SYNTAX_TREE_KEYS = ["med_st_ALL_sum", "med_st_NP_sum"]
+    _READABILITY_KEYS = ["read_gunning-fog_sum", "read_coleman-liau_sum", "read_dale-chall_sum", "read_flesch-kincaid_sum", "read_linsear-write_sum", "read_spache_sum", "read_automatic_sum", "read_flesch_sum"]
+
+    @dataclass
+    class NonLatentConfig:
+        diversity: bool = True
+        pronoun: bool = True
+        quantity: bool = True
+        sentiment: bool = True
+        average: bool = True
+        median_syntax_tree: bool = True
+        readability: bool = True
+
+        def build_keys(self):
+            keys = []
+            if self.diversity:
+                keys.extend(Pipeline._DIVERSITY_KEYS)
+            if self.pronoun:
+                keys.extend(Pipeline._PRONOUN_KEYS)
+            if self.quantity:
+                keys.extend(Pipeline._QUANTITY_KEYS)
+            if self.sentiment:
+                keys.extend(Pipeline._SENTIMENT_KEYS)
+            if self.average:
+                keys.extend(Pipeline._AVERAGE_KEYS)
+            if self.median_syntax_tree:
+                keys.extend(Pipeline._MEDIAN_SYNTAX_TREE_KEYS)
+            if self.readability:
+                keys.extend(Pipeline._READABILITY_KEYS)
+            return keys
+
+        def build_drop_keys(self):
+            keys = []
+            if not self.diversity:
+                keys.extend(Pipeline._DIVERSITY_KEYS)
+            if not self.pronoun:
+                keys.extend(Pipeline._PRONOUN_KEYS)
+            if not self.quantity:
+                keys.extend(Pipeline._QUANTITY_KEYS)
+            if not self.sentiment:
+                keys.extend(Pipeline._SENTIMENT_KEYS)
+            if not self.average:
+                keys.extend(Pipeline._AVERAGE_KEYS)
+            if not self.median_syntax_tree:
+                keys.extend(Pipeline._MEDIAN_SYNTAX_TREE_KEYS)
+            if not self.readability:
+                keys.extend(Pipeline._READABILITY_KEYS)
+            return keys
+
+    def __init__(self, similarity=True, non_latent: Optional[NonLatentConfig]=NonLatentConfig()):
         self.similarity = similarity
-        self.sentiment = sentiment
+        self.non_latent = non_latent
     
     def load_dataset_from_file(self, dir = "data/Horne2017_FakeNewsData/Buzzfeed"):
         base = Path(dir)
@@ -36,8 +93,11 @@ class Pipeline:
             labels_to_drop = ["label"]
             if not self.similarity:
                 labels_to_drop.append("tf_idf_1_2_harmonic_mean")
-            if not self.sentiment:
-                labels_to_drop.extend(("subjectivity", "polarity"))
+            if self.non_latent:
+                labels_to_drop.extend(self.non_latent.build_drop_keys())
+            else:
+                # Delete all keys
+                labels_to_drop.extend(Pipeline.NonLatentConfig().build_keys())
             X = df.drop(labels_to_drop, axis=1).to_numpy()
             sets[name] = (X, y)
         X_train, y_train = sets["train"]
@@ -58,32 +118,30 @@ class Pipeline:
         Returns:
             (X: np.ndarray, y: np.ndarray): A numpy X and y with all the features concatenated
         """
-        logging.basicConfig(format='\x1b[1;36m%(asctime)s: %(message)s\u001b[0m', level=logging.INFO, datefmt='%Y-%m-%dT%H:%M:%SZ')
+        logging.basicConfig(format="\x1b[1;36m%(asctime)s: %(message)s\u001b[0m", level=logging.INFO, datefmt="%Y-%m-%dT%H:%M:%SZ")
         logging.getLogger().disabled = quiet
 
         # Load dataset
         df = DatasetLoader().load_horne2017_fakenewsdata(drop_if_less_than_num_contexts=1).as_pandas()
 
-        # Sentiment features (handles unprocessed text)
-        if self.sentiment:
-            logging.info("Sentiment extraction...")
-            sentiment = TextBlobSentimentExtractor()
-            def extract_sentiment(row):
-                s_content = sentiment.extract_sentiment(row["content"])
-                delta_subjectivity = []
-                delta_polarity = []
-                for key in ("ctx1_content", "ctx2_content", "ctx3_content"):
-                    s = sentiment.extract_sentiment_opt(row[key])
-                    if s.avg_subjectivity is not None:
-                        delta_subjectivity.append(s_content.avg_subjectivity - s.avg_subjectivity)
-                        delta_polarity.append(s_content.avg_polarity - s.avg_polarity)
-                subjectivity = sum(delta_subjectivity) / len(delta_subjectivity)
-                polarity = sum(delta_polarity) / len(delta_polarity)
-                return pd.Series((subjectivity, polarity), ("subjectivity", "polarity"))
-            df = pd.concat([df, df.apply(extract_sentiment, axis=1)], axis=1)
-            logging.info("Sentiment extraction...done")
+        # Non latent features (requires limited preprocessing)
+        if self.non_latent:
+            logging.info("Non latent feature extraction...")
+            preprocessor_ascii = Preprocessor(
+                lowercase=False,
+                remove_non_ascii=True,
+                remove_punctuation=False,
+                lemmatization=False,
+                remove_stopwords=False
+            )
+            def extract_non_latent(row):
+                content = preprocessor_ascii.preprocess(row["content"])
+                non_latent_dict = NonLatentFeatures(content).output_all()
+                return pd.Series(non_latent_dict.values(), non_latent_dict.keys())
+            df = pd.concat([df, df.apply(extract_non_latent, axis=1)], axis=1)
+            logging.info("Non latent feature extraction...done")
         else:
-            logging.info("Sentiment extraction...skipped")
+            logging.info("Non latent feature extraction...skipped")
 
         # Preprocess data
         logging.info("Preprocessing and tokenization...")
@@ -121,12 +179,15 @@ class Pipeline:
             logging.info("Similarity comparison...skipped")
 
         # Convert to numpy
+        feature_labels = [f"bert_{n}" for n in range(len(df["content_bert"][0]))]
         X = np.vstack(df["content_bert"])
         if self.similarity:
+            feature_labels.append("tf_idf_1_2_harmonic_mean")
             X = np.hstack((X, df["tf_idf_1_2_harmonic_mean"].to_numpy().reshape((-1, 1))))
-        if self.sentiment:
-            X = np.hstack((X, df["subjectivity"].to_numpy().reshape((-1, 1))))
-            X = np.hstack((X, df["polarity"].to_numpy().reshape((-1, 1))))
+        if self.non_latent:
+            non_latent_keys = self.non_latent.build_keys()
+            feature_labels.extend(non_latent_keys)
+            X = np.hstack((X, df[non_latent_keys].to_numpy()))
         y = df["label"].apply(int).to_numpy()
 
         # Datasets: 60, 20, 20 split on train, valid, test
@@ -134,18 +195,27 @@ class Pipeline:
         X_train, X_valid, y_train, y_valid = train_test_split(X1, y1, test_size=0.25, random_state=random_state, stratify=y1)
 
         # Scale features
-        scaler = MinMaxScaler(feature_range=(0, 1))
-        X_train = scaler.fit_transform(X_train)
-        X_valid = scaler.transform(X_valid)
-        X_test = scaler.transform(X_test)
+        needs_standard_scaler = []
+        needs_min_max_scaler = []
+        for i, label in enumerate(feature_labels):
+            if label.endswith("_sum"):
+                needs_standard_scaler.append(i)
+            else:
+                needs_min_max_scaler.append(i)
+        
+        standard_scaler = StandardScaler()
+        X_train[:, needs_standard_scaler] = standard_scaler.fit_transform(X_train[:, needs_standard_scaler])
+        X_valid[:, needs_standard_scaler] = standard_scaler.transform(X_valid[:, needs_standard_scaler])
+        X_test[:, needs_standard_scaler] = standard_scaler.transform(X_test[:, needs_standard_scaler])
+
+        min_max_scaler = MinMaxScaler(feature_range=(0, 1))
+        X_train[:, needs_min_max_scaler] = min_max_scaler.fit_transform(X_train[:, needs_min_max_scaler])
+        X_valid[:, needs_min_max_scaler] = min_max_scaler.transform(X_valid[:, needs_min_max_scaler])
+        X_test[:, needs_min_max_scaler] = min_max_scaler.transform(X_test[:, needs_min_max_scaler])
 
         if save_dir:
             base = Path(save_dir)
-            labels = ["label", *[f"bert_{n}" for n in range(len(df["content_bert"][0]))]]
-            if self.similarity:
-                labels.append("tf_idf_1_2_harmonic_mean")
-            if self.sentiment:
-                labels.extend(("subjectivity", "polarity"))
+            labels = ["label", *feature_labels]
             for (name, XX, yy) in [("train", X_train, y_train), ("valid", X_valid, y_valid), ("test", X_test, y_test)]:
                 df_out = pd.DataFrame(np.hstack((yy.reshape((-1, 1)), XX)), columns=labels)
                 df_out.to_csv(base.joinpath(f"features_{name}.csv"), index=False)
