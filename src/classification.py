ec697c445d5150c3787a9e3a817997269b1283e6
Jim Tang <jim.jim.tang@hotmail.com>
2023-08-05 21:30:20 +1000

    classification: Remove class weight on svc

--- src/classification.py
+++ src/classification.py
@@ -335,7 +335,6 @@ class MachineLearningClassifier:
                     "C": [0.2, 0.4, 0.6, 0.8, 1.0, 1.2],
                     "kernel": ["rbf", "poly", "sigmoid"],
                     "gamma": ["scale", 0.01, 0.05],
-                    "class_weight": [None, "balanced"]
                 },
                 n_jobs=-1,
                 scoring="f1"
